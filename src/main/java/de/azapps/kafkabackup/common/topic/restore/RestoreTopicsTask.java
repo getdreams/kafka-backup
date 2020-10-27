e09fa467e8604302659020742e252d316b6e6249
MacRudDNA <Maciej.Rudnicki@digitalnewagency.com>
2020-10-27 10:51:33 +0100

    Added arg len assertion

--- src/main/java/de/azapps/kafkabackup/common/topic/restore/RestoreTopicsTask.java
+++ src/main/java/de/azapps/kafkabackup/common/topic/restore/RestoreTopicsTask.java
@@ -1,8 +1,16 @@
 package de.azapps.kafkabackup.common.topic.restore;
 
+import com.sun.tools.javac.util.ArrayUtils;
+import java.util.Arrays;
+
 public class RestoreTopicsTask {
 
   public static void main(String[] args) {
+    if (args.length != 1) {
+      throw new RuntimeException(String.format("Found %d arguments. Expecting exactly one. Arguments: %s",
+          args.length, Arrays.toString(args)));
+    }
+
     System.out.println("Restoring topics with config file: " + args[0]);
 
     RestoreTopicsArgsWrapper restoreTopicsArgsWrapper = new RestoreTopicsArgsWrapper();
